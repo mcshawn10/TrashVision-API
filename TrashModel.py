a82e48f6fdeae2f766572ec60990f096cbacf589
shawn <shawnmcneill10@gmail.com>
2023-08-07 21:16:11 -0500

    class and data processing, need to create pipeline

--- TrashModel.py
+++ TrashModel.py
@@ -1,17 +1,29 @@
-import torch
+#import torch
 import torchvision
+import torch.nn as nn
+import torchvision.models as models
 
 
 
-MobileNet = torchvision.models.mobilenet_v2()
-
-class TrashModel(torch.nn.Module):
+class TrashModel(nn.Module):
     def __init__(self, gpu):
-        super(MobileNet, self).__init__()
+        super(TrashModel, self).__init__()
         self.to(gpu)
-        
+        self.NUM_CLASSES = 12
+
+        self.mobilenet = models.mobilenet_v2(pretrained=False)  
+        in_features = self.mobilenet.classifier[1].in_features
+        self.mobilenet.classifier = nn.Sequential(nn.Linear(in_features, self.NUM_CLASSES))
+    
+    def forward(self, x):
+        return self.mobilenet(x)   
+    
     def build(self):
         pass
 
     def train(self):
         pass
+
+
+if __name__ ==  "__main__":
+    pass
