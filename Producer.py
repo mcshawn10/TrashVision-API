1b0f94d129d4166d980d020397235745b58b7d26
shawn <shawnmcneill10@gmail.com>
2023-09-06 16:20:10 -0500

    creating kafka scripts

--- Producer.py
+++ Producer.py
@@ -1,14 +1,18 @@
 from confluent_kafka import Producer
 
-class KafkaProducer:
-    def __init__(self) -> None:
-        self.producer_config = {
-                'bootstrap.servers': 'your_kafka_broker_address',
-                'security_protocol': "...",
+# class KafkaProducer:
+#     def __init__(self) -> None:
+#         self.producer_config = {
+#                 'bootstrap.servers': 'localhost',
+#                 'security_protocol': "...",
                 
-            }
+#             }
 
-        self.producer = Producer(self.producer_config)
+#         self.producer = Producer(self.producer_config)
+
+producer_config = {'bootstrap.servers': 'localhost:9092'
+                   }
+producer = Producer(producer_config)
 
 
 
---
+++ startTopic.sh
@@ -0,0 +1,1 @@
+sudo /opt/kafka/bin/./kafka-topics.sh --create --topic TrashVisionTopic --bootstrap-server localhost:9092 --partitions 1 --replication-factor 1
